d990f1361756efad01c07fa7faf0e3006828649e
pupil <pupil@ivkhk.ee>
2022-10-18 15:15:12 +0300

    first commit

---
+++ src/entity/Book.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author pupil
+ */
+public class Book {
+    private String title;
+    private Author[] authors;
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+    
+    public void addAuthor(Author auhtor){
+        Author[] newAuthors = Arrays.copyOf(authors, authors.length+1);
+        newAuthors[newAuthors.length-1]=auhtor;
+        this.authors = newAuthors;
+    }
+    
+    public void removeAuthor(int numberOfAuthor){
+        //обнуляем указанного автора (по индексу)
+        this.getAuthors()[numberOfAuthor-1]=null;
+        //создаем массив с количеством элементов на 1 меньше
+        Author[] newAuthors = new Author[this.getAuthors().length-1];
+        // в цикле копируем элементы в новый массив не учитывая обнуленную ячейку
+        int j = 0;
+        for (Author author : this.getAuthors()) {
+            if (author != null) {
+                newAuthors[j] = author;
+                j++;
+            }
+        }
+        //копируем ссылку на новый массив в книгу
+        this.setAuthors(newAuthors);
+    }
+    @Override
+    public String toString() {
+        return "Book{" 
+                + "title=" + title 
+                + ", authors=" + Arrays.toString(authors) 
+                + '}';
+    }
+    
+}
